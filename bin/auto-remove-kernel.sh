394cfcf5a653d3e7c3386283db5be5fe7fb2ad8b
Davide Ragaini <ragainidavide@gmail.com>
2023-10-30 23:01:07 +0100

    Late Late October

--- bin/auto-remove-kernel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-KERNELS="$(vkpurge list)"
-[ "$(echo "$KERNELS" | wc -l)" -gt 2 ] &&
-    vkpurge remove "$(echo "$KERNELS" | head -n -2 | tr '\n' ' ')"
---
+++ bin/nix/matlab.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+case "$1" in
+    broken) nix-shell ~/sources/nix-matlab/shell.nix --run "$(printf "%s" "$(nix-build)/bin/matlab -desktop -nosplash -sd $HOME/Uni/Tirocinio/Appunti/Matlab -logfile=$HOME/MATLAB/LogFiles/matlab.log -nosoftwareopengl")" ;;
+    *) cd ~/sources/nix-matlab && nix run -v ;;
+esac
