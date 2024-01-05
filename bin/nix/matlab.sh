815b6d608fc997641dd6810f21f41f227c1910bc
Davide Ragaini <ragainidavide@gmail.com>
2024-01-05 20:20:26 +0100

    Middle Early January

--- bin/nix/matlab.sh
+++ bin/nix/matlab.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 case "$1" in
     broken) nix-shell ~/sources/nix-matlab/shell.nix --run "$(printf "%s" "$(nix-build)/bin/matlab -desktop -nosplash -sd $HOME/Uni/Tirocinio/Appunti/Matlab -logfile=$HOME/MATLAB/LogFiles/matlab.log -nosoftwareopengl")" ;;
-    *) cd ~/sources/nix-matlab && nix run -v ;;
+    *) cd ~/sources/nix-matlab &&
+             git pull &&
+             nix run -v ;;
 esac
+notify-send "Matlab terminated"
