6bec2f5f3ddeac61caa525f17a7dadae3bfcf3d8
Davide Ragaini <ragainidavide@gmail.com>
2024-05-05 16:04:41 +0200

    Middle Early May

--- bin/nix/matlab.sh
+++ bin/nix/matlab.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env sh
+
+DIR="$HOME/sources/nix-matlab"
+LAST_UPDATE="$(date +%s -r "$DIR"/flake.lock)"
+[ "$((LAST_UPDATE + 604800))" -le "$(date +%s)" ] &&
+    git -C "$DIR" pull
+# nix run -v -run > ~/matlab.log
+# nix-shell "$DIR"/shell.nix --run "$(printf "%s" "$(nix-build)/bin/matlab -desktop -nosplash -sd $HOME/uni/Tirocinio/Appunti/Matlab -logfile=$HOME/MATLAB/LogFiles/matlab.log -nosoftwareopengl")"
+
 case "$1" in
-    broken) nix-shell ~/sources/nix-matlab/shell.nix --run "$(printf "%s" "$(nix-build)/bin/matlab -desktop -nosplash -sd $HOME/Uni/Tirocinio/Appunti/Matlab -logfile=$HOME/MATLAB/LogFiles/matlab.log -nosoftwareopengl")" ;;
-    *) cd ~/sources/nix-matlab &&
-             git pull &&
-             nix run -v ;;
+    'shell')
+        nix-shell "$DIR"/shell.nix --run "$(printf "%s" "$(nix-build)/bin/matlab -desktop -nosplash -sd $HOME/uni/Tirocinio/Appunti/Matlab -logfile=$HOME/MATLAB/LogFiles/matlab.log -nosoftwareopengl")"
+        ;;
+    '-desktop' | 'run' | *)
+        cd "$DIR" &&
+            nix run -v >~/matlab.log
+        ;;
 esac
+# case "$1" in
+#     broken) nix-shell ~/sources/nix-matlab/shell.nix --run "$(printf "%s" "$(nix-build)/bin/matlab -desktop -nosplash -sd $HOME/Uni/Tirocinio/Appunti/Matlab -logfile=$HOME/MATLAB/LogFiles/matlab.log -nosoftwareopengl")" ;;
+#     *) cd ~/sources/nix-matlab &&
+#              git pull &&
+#              nix run -v ;;
+# esac
 notify-send "Matlab terminated"
