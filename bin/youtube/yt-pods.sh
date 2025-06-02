4a0ee2ab40f68e2f6df3cef78a78faa9ac13427a
Davide Ragaini <ragainidavide@gmail.com>
2025-06-02 23:18:00 +0200

    Fixes

--- bin/youtube/yt-pods.sh
+++ bin/youtube/yt-pods.sh
@@ -18,15 +18,16 @@ set -x
 find "$root_dir/raw" -mindepth 2 -type d -not -path "*/speedup/*" | while read dir; do
         pushd "$dir" || exit
         mkdir -p speedup
-        parallel -j 3 ffmpeg -i {} -filter 'atempo=1.25' speedup/{} ::: *.mp3
+        parallel -j 3 ffmpeg -i {} -filter 'atempo=1.25' speedup/{} ::: *.mp3 &&
         pushd speedup || exit
-        parallel -j 3 ffmpeg-normalize {} -v --progress --auto-lower-loudness-target -c:a libmp3lame -ext mp3 ::: *.mp3
+        parallel -j 3 ffmpeg-normalize {} -v --progress --auto-lower-loudness-target --keep-lra-above-loudness-range-target -c:a libmp3lame -ext mp3 ::: *.mp3 &&
         popd || exit
         popd || exit
         name="$(echo "$dir" | cut -d'/' -f6-)"
-        mkdir -p "$root_dir/ready"
-        mv "$dir/speedup/normalized" "$root_dir/ready/$name"
-        mv "$dir" "$root_dir/archive/"
+        mkdir -p "$root_dir/archive/$name"
+        mkdir -p "$root_dir/ready/$name"
+        mv "$dir/speedup/normalized/" "$root_dir/ready/$name"
+        mv "$dir" "$root_dir/archive/$name"
 done
         # mkdir -p "$dir/speedup" && parallel ffmpeg -i {} -filter 'atempo=1.25' "$dir"/speedup/{} ::: * && cd speedup && parallel ffmpeg-normalize {} -v --progress --auto-lower-loudness-target -ext mp3 ::: "$dir/*.mp3"
         # parallel ffmpeg-normalize {} -v --progress --auto-lower-loudness-target -ext mp3 ::: "$dir/*.mp3"; done
